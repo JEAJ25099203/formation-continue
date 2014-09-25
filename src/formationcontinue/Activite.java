2a5c54566c759cb33a009d1b73f5909edb8e29eb
Firas Cherif <firas-cherif@hotmail.com>
2014-09-25 14:53:38 -0400

    indentation3

--- src/formationcontinue/Activite.java
+++ src/formationcontinue/Activite.java
@@ -3,68 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package formationcontinue;
 
 import net.sf.json.JSONObject;
-
 
 /**
  *
  * @author Bruno
  */
-public class Activite{
+public class Activite {
 
-	private String description;
-	private String categorie;
-	private int heures;
-	private String date;
+    private String description;
+    private String categorie;
+    private int heures;
+    private String date;
 
-	public Activite(){
-            
-	}
-        
-        public Activite(JSONObject json){
-            this.description = json.getString("description");
-            this.categorie = json.getString("categorie");
-            this.heures = json.getInt("heures");
-            this.date = json.getString("date");
-        }
+    public Activite() {
+    }
 
-	public String getDescription(){
-		return description;
-	}
+    public Activite(JSONObject json) {
+        this.description = json.getString("description");
+        this.categorie = json.getString("categorie");
+        this.heures = json.getInt("heures");
+        this.date = json.getString("date");
+    }
 
-	public String getCategorie(){
-		return categorie;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public int getHeures(){
-		return heures;
-	}
+    public String getCategorie() {
+        return categorie;
+    }
 
-	public String getDate(){
-		return date;
-	}
+    public int getHeures() {
+        return heures;
+    }
 
-	public void setDescription(String description){
-		this.description = description;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public void setCategorie(String categorie){
-		this.categorie = categorie;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setHeures(int heures){
-		this.heures = heures;
-	}
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
 
-	public void setDate(String date){
-		this.date = date;
-	}
-        
-        @Override
-        public String toString(){
-            return description + "\n" + categorie + "\n" + heures + "\n" + date + "\n";
-        }
+    public void setHeures(int heures) {
+        this.heures = heures;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return description + "\n" + categorie + "\n" + heures + "\n" + date + "\n";
+    }
 }
